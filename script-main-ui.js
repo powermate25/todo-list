d636eecd53de796ef27c3ddabe47d48bd0d5d172
powermate <218421152+powermate25@users.noreply.github.com>
2025-12-01 19:40:30 +0000

    Routine save: Todo List project progress - UI elements fix

---
+++ script-main-ui.js
@@ -0,0 +1,41 @@
+console.log("Working!")
+
+// Date Display
+const dateDiv = document.querySelector(".date-now")
+const timeDiv = document.querySelector(".time-now")
+
+setInterval(
+    () => {
+        let timeNow = new Date()
+        let currentDate = timeNow.toDateString()  + ", "
+        let currentTime = timeNow.getHours() + ":" + timeNow.getMinutes() + ":" + timeNow.getSeconds()
+        dateDiv.textContent = currentDate
+        timeDiv.textContent = currentTime
+
+   // clog("tick")
+    }, 1000)
+
+// Todo items display
+const todoDiv = document.querySelector(".todo-item")
+const taskTitle = document.querySelector(".title")
+const taskDescription = document.querySelector(".description")
+const taskStatus = document.querySelector(".status .status-value")
+const taskPriority = document.querySelector(".priority .priority-value")
+
+const editTaskBtn = document.querySelector(".edit-button")
+const deleteTaskBtn = document.querySelector(".delete-button")
+
+displayItem = function(){
+    const todoDiv = document.createElement("div")
+    todoDiv.className = "todo-item"
+    const taskTitle = document.createElement("div")
+    taskTitle.className = "title"
+    const taskDescription = document.createElement("div")
+    taskDescription.className = "description"
+    const taskStatus = document.createElement("div")
+    taskStatus.className = ("status-value")
+
+
+}
+
+ 
